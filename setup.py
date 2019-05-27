22fa49986abe98d1fc1bf9dc84d50ed1af683800
mmf1982 <5464@gmx.net>
2019-05-27 12:21:34 +0200

    initial version

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup(
+    name="pyhdfview",
+    version="0.0.1",
+    author="Martina M Friedrich",
+    author_email="5464@gmx.net",
+    description="A package to interactively view netCDF and hdf4/ hdf5 files",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mmf1982/pyhdfview/tree/master",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
+view raw
+
